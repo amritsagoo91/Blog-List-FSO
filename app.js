108c1e825af5dc4436415d6d864f0b307bfd8ce3
amritsagoo91 <amritsagoo91@gmail.com>
2025-06-10 16:22:09 -0700

    4.2 Blog List, step 2

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const blogRouter = require('./controller/blog')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const { MONGODB_URI } = require('./utils/config')
+
+
+const app = express()
+
+
+mongoose.connect(MONGODB_URI).then(() => {
+    logger.info("Connected Successfully")
+}).catch((error) => logger.error(error.message))
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogRouter)
+app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+
+
+module.exports = app
---
+++ models/blog.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+})
+
+module.exports = new mongoose.model('Blog', blogSchema, 'blogs')
