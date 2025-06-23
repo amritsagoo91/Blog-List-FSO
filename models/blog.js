7c054749dbd6455c0487882c119aa6dd05cbf553
amritsagoo91 <amritsagoo91@gmail.com>
2025-06-23 13:26:06 -0700

    4.9: Blog List Tests, step 2

--- models/blog.js
+++ models/blog.js
@@ -8,4 +8,12 @@ const blogSchema = mongoose.Schema({
   likes: Number,
 })
 
-module.exports = new mongoose.model('Blog', blogSchema)
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema, 'blogs')
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -31,6 +31,14 @@ test.only('all blogs are returned', async () => {
     assert.strictEqual(response.body.length, 2)
 })
 
+test.only('unique identifier property of blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
+
+    const blog = response.body[0]
+    assert.ok(blog.id)
+    assert.strictEqual(blog._id, undefined)
+})
+
 after(async () => {
     await mongoose.connection.close()
 })
